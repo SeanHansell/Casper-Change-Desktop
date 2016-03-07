001b3276ed9d759000d0915f4e808a17921aba70
Sean Hansell <sean@morelen.net>
2016-03-07 10:10:23 -0500

    First major code change. Removed dual-desktop functionality. Variablized parameter 4 to desktop_picture. Parameter is no longer a binary switch, but the path to the Desktop Picture you want to set.

--- SetDesktopBackground.sh
+++ SetDesktopBackground.sh
@@ -2,41 +2,34 @@
 
 # Script to set desktop background for students
 
-# Author : r.purves@arts.ac.uk
+# Author : Richard Purves <r.purves@arts.ac.uk>, Sean Hansell <sean@morelen.net>
 # Version 1.0 : Initial Version
-# Version 1.1 : 23/04/2014 - Massive reworking to use applescript for 10.8 and below, modify the db for 10.9+
-# Version 1.2 : 24/04/2014 - Removed applescript because of osascript parsing issues. replaced with mcx.
+# Version 1.1 : 2014-04-23 - Massive reworking to use applescript for 10.8 and below, modify the db for 10.9+
+# Version 1.2 : 2014-04-24 - Removed applescript because of osascript parsing issues. replaced with mcx.
+# Version 1.3 : 2016-03-07 - Overhaul to variablize the desktop picture path.
 
+desktop_picture="${4}"
 
 os_version=$( sw_vers | grep ProductVersion: | awk '{print $2}' | sed 's/\./\ /g' | awk '{print $2}' )
 current_user=$( ls -l /dev/console | awk '{print $3}' )
-current_user_home=$( dscl . -read /Users/sean NFSHomeDirectory | sed 's/NFSHomeDirectory\:\ //' )
+current_user_home=$( dscl . -read "/Users/${current_user}" NFSHomeDirectory | sed 's/NFSHomeDirectory\:\ //' )
 
-if [[ "$4" = "custom" ]]
+if [[ -z "${desktop_picture}" ]]
 then
-	if (( $os_version > 8 ))
-	then
-		sqlite3 "/Users/${current_user_home}/Library/Application Support/Dock/desktoppicture.db" << EOF
-			UPDATE data SET value = "/Users/Shared/Background/custombg.jpeg";
-			.quit
-			EOF
-		killall Dock
-	else
-		defaults write com.apple.desktop Background '{default = {ImageFilePath = "/Users/Shared/Background/custombg.jpeg"; };}'
-		killall Dock
-	fi
-else
-	if (( $os_version > 8 ))
-	then
-		sqlite3 "/Users/${current_user_home}/Library/Application Support/Dock/desktoppicture.db" << EOF
-			UPDATE data SET value = "/Library/Desktop Pictures/default_grey2560x1600.jpeg";
-			.quit
-			EOF
-		killall Dock
-	else
-		defaults write com.apple.desktop Background '{default = {ImageFilePath = "/Library/Desktop Pictures/default_grey2560x1600.jpeg"; };}'
-		killall Dock
-	fi
+	exit 1
 fi
+
+if (( $os_version > 8 ))
+then
+	sqlite3 "${current_user_home}/Library/Application Support/Dock/desktoppicture.db" << EOF
+		UPDATE data SET value = "${desktop_picture}";
+		.quit
+	EOF
+else
+	defaults delete com.apple.desktop Background
+	defaults write com.apple.desktop Background '{default = {ImageFilePath = "'"${desktop_picture}"'";};}'
+fi
+
+killall Dock
 
 exit 0
